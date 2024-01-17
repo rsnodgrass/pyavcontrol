452d8149f7bb4577bb7aacfd4876c9a74650c1b1
Ryan Snodgrass <rsnodgrass@gmail.com>
2024-01-17 08:35:10 -0800

    update

--- tests/test_device_client.py
+++ tests/test_device_client.py
@@ -1,5 +1,6 @@
 """
-Tests and example of a dynamically created Activity classes
+Tests several interfaces of the MX-160 to confirm that typical usage patterns of a
+DeviceClient work.
 """
 
 import asyncio
@@ -11,7 +12,11 @@ from pyavcontrol import ZoneStatus, get_async_mcintosh, get_mcintosh
 from tests import create_dummy_port
 
 
-class TestMcIntoshMx160(unittest.TestCase):
+class TestDeviceClient(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        pass
+    
     def setUp(self):
         self.library = None
         self.client = None
@@ -21,21 +26,21 @@ class TestMcIntoshMx160(unittest.TestCase):
             def send():
                 pass
         
-
-    def test_set_mute(self):
-        #self.client.mute.on()
-        #self.client.mute.off()
-        #self.assertEqual(0, len(self.responses))
+    def test_request_with_response(self):
         pass
 
-class TestAsyncXantech8(TestXantech8):
-    def setUp(self):
-        self.responses = {}
+    def test_request_without_response(self):
+        pass
 
     def test_timeout(self):
         with self.assertRaises(asyncio.TimeoutError):
-            self.mcintosh.set_source(3, 3)
+            self.client.power.turn_off()
 
+    def test_missing_arguments(self):
+        pass
+
+    def test_response_dictionary_parsing(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
